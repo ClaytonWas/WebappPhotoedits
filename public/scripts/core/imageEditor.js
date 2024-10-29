9fe7b00741267029cfb3debacfd390e7f339b3ee
ClaytonWas <claywasmund@gmail.com>
2024-10-29 00:03:04 -0400

    Nearest Neighbour Interpolation now works with the reworked  ImageEditor class

--- public/scripts/core/imageEditor.js
+++ public/scripts/core/imageEditor.js
@@ -24,7 +24,7 @@ export class ImageEditor {
                 Type:           image/png 
         */
  
-    
+        this.image = this.IMAGE                     // Used to resize images from from the original image dimensions while allow overwrites. 
         this.canvas = canvas
         this.context = canvas.getContext("2d")
 
@@ -36,7 +36,7 @@ export class ImageEditor {
     }
 
     // Load image loads a canvas with the passed image.
-    // TODO: It's important to do this to allow the user to crop the image at a later date. (I will need to change this.image calls to something more relative.)
+    // TODO: It's important to do this to allow the user to crop the image at a later date. (I will need to change this.IMAGE calls to something more relative.)
     // TODO: This should also avoid the problem of reloading an image, and it should allow applyLayers() to start at the index of the current image without redrawing everything below it.
     loadImage() {
         this.context.drawImage(this.IMAGE, 0, 0)
@@ -50,7 +50,7 @@ export class ImageEditor {
     }
 
     renderImage() {
-        this.modifiedImage = this.IMAGE
+        this.modifiedImage = this.image
         this.context.drawImage(this.modifiedImage, 0, 0)
         const imageData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height)
         
@@ -63,39 +63,27 @@ export class ImageEditor {
 
     }
 
-    nearestNeighbourInterpolation(newHeight, newWidth) {
+    nearestNeighbourInterpolation(newWidth, newHeight) {
         const tempCanvas = document.createElement('canvas');
         const tempContext = tempCanvas.getContext('2d');
+
         tempCanvas.width = newWidth;
         tempCanvas.height = newHeight;
 
-        for (let y = 0; y < newHeight; y++) {
-            for (let x = 0; x < newWidth; x++) {
-                // Calculate the position in the original image
-                const originalX = Math.floor((x * this.IMAGE.width) / newWidth);
-                const originalY = Math.floor((y * this.IMAGE.height) / newHeight);
+        this.image = this.IMAGE
+        tempContext.drawImage(this.image, 0, 0, newWidth, newHeight); //I had no clue draw image had prebuilt interpolation, im just doing this instead wow
 
-                // Get the pixel color from the original image
-                const pixelData = this.context.getImageData(originalX, originalY, 1, 1).data;
-
-                // Set the pixel color in the new image
-                tempContext.fillStyle = `rgba(${pixelData[0]}, ${pixelData[1]}, ${pixelData[2]}, ${pixelData[3] / 255})`;
-                tempContext.fillRect(x, y, 1, 1);
-            }
-        }
-
-        // Update core image to the new height and width.
-        let resizedImage = new Image()
-        resizedImage.src = tempCanvas.toDataURL(this.TYPE)
-        this.IMAGE = resizedImage
-
-        this.canvas.height = newHeight
-        this.canvas.width = newWidth
-        this.context.drawImage(tempCanvas, 0, 0);
+        // Create new Image from the tempCanvas Context
+        let resizedImage = new Image();
+        resizedImage.src = tempCanvas.toDataURL(this.TYPE);
+        resizedImage.onload = () => {
+            this.image = resizedImage;
+            this.canvas.width = newWidth;
+            this.canvas.height = newHeight;
+            this.context.drawImage(resizedImage, 0, 0);
+        };
     }
 
-    // This changes the resolution of this.image. Can be dangerous as we rely on this.image being an accurate representation of the provided image.
-    // The reason we change this.image has to do this this.renderImage()
     resizeCanvas(newHeight, newWidth, maintainAspectRatio, interpolationType) {
         console.log('Passing through: ', newHeight, newWidth, maintainAspectRatio, interpolationType)
 
